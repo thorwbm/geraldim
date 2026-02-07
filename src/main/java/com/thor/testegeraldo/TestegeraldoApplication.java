7d777dee63aba3b03ada62a427e71b3f4800b326
wemerson maduro <lagoawb@gmail.com>
2026-02-07 13:47:18 -0300

    todos

---
+++ src/main/java/com/thor/testegeraldo/TestegeraldoApplication.java
@@ -0,0 +1,13 @@
+package com.thor.testegeraldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestegeraldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestegeraldoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thor/testegeraldo/TestegeraldoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thor.testegeraldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestegeraldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
